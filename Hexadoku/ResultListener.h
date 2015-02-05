090d009eb6237b38b91edf312f11accc0e8f9122
Bill Farmer <williamjfarmer@yahoo.co.uk>
2015-02-05 10:18:13 +0000

    "Add copyright

--- Hexadoku/ResultListener.h
+++ Hexadoku/ResultListener.h
@@ -1,12 +1,23 @@
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 //
-//   DancingLinks.cpp  1.0  26 May 2006
+//  Sudoku - Sudoku solver.
 //
-//   Dancing Links - solve puzzles using Dancing Links algorithm by
-//   Donald Knuth. See academic paper 'Dancing Links', Donald
-//   E. Knuth, Stanford University.
+//  Copyright (C) 2015	Bill Farmer
 //
-//   Copyright (C) 2006 Bill Farmer
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Bill Farmer	 william j farmer [at] yahoo [dot] co [dot] uk.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
